eedb7a1e0f995001b159191fbcfb519b5b730b88
David Heimann <d@dmeh.net>
2020-04-26 13:58:14 -0400

    Try setting https for static assets url in config

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :home, HomeWeb.Endpoint,
   url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
+  static_url: [scheme: "https", host: System.get_env("RENDER_EXTERNAL_HOSTNAME"), port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
