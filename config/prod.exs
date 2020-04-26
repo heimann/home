52b4334905f33c23535a170951066547de989d1e
David Heimann <d@dmeh.net>
2020-04-26 19:23:13 -0400

    Fix syntax error in prod.exs (oops)

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :home, HomeWeb.Endpoint,
-  url: [host: "www.dmeh.net", port: 80],
-  static_url: [scheme: "https", "www.dmeh.net", port: 443],
+  url: [host: "www.dmeh.net" || "localhost", port: 80],
+  static_url: [scheme: "https", host: "www.dmeh.net", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
