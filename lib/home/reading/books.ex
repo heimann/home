e2f76ff5bea7a03b1dd2ca1caf4dcda7197d8380
David Heimann <d@dmeh.net>
2020-08-01 16:46:54 -0400

    Add some sci-fi books

--- lib/home/reading/books.ex
+++ lib/home/reading/books.ex
@@ -53,6 +53,77 @@ defmodule Home.Reading.Books do
       url:
         "https://www.goodreads.com/book/show/29506.Prisoner_s_Dilemma?from_search=true&from_srp=true&qid=JsqVJ7l8iO&rank=2",
       isbn: "9780385415804"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "Last and First Men",
+      author: "Olaf Stapledon",
+      url:
+        "https://www.goodreads.com/book/show/2749148-last-and-first-men?from_search=true&from_srp=true&qid=in3KImOtXo&rank=2",
+      isbn: "9780486466828"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "The Gods Themselves",
+      author: "Isaac Asimov",
+      url:
+        "https://www.goodreads.com/book/show/41821.The_Gods_Themselves?ac=1&from_search=true&qid=rOZh4M8O6U&rank=1",
+      isbn: "9781857989342"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "Solaris",
+      author: "Stanislaw Lem",
+      url:
+        "https://www.goodreads.com/book/show/95558.Solaris?from_search=true&from_srp=true&qid=jJeseuaDh9&rank=1",
+      isbn: "9780156837507"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "Roadside Picnic",
+      author: "Arkady & Boris Strugatsky",
+      url: "https://www.goodreads.com/book/show/331256.Roadside_Picnic",
+      isbn: "9780575070530"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "The Invincible",
+      author: "Stanislaw Lem",
+      url:
+        "https://www.goodreads.com/book/show/251633.The_Invincible?from_search=true&from_srp=true&qid=15kAckSbQn&rank=3",
+      isbn: "9780283979620"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "Dispora",
+      author: "Greg Egan",
+      url:
+        "https://www.goodreads.com/book/show/156785.Diaspora?ac=1&from_search=true&qid=E1EThJXSc3&rank=1",
+      isbn: "9783453161818"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "The Three-Body Problem Trilogy",
+      author: "Cixin Liu",
+      url:
+        "https://www.goodreads.com/book/show/52073230-three-body-problem-series-3-books-collection-set---the-dark-forest-deat?from_search=true&from_srp=true&qid=mspL9q5CST&rank=9",
+      isbn: "9789123663026"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "Rendezvous with Rama",
+      author: "Arthur C. Clarke",
+      url:
+        "https://www.goodreads.com/book/show/112537.Rendezvous_with_Rama?ac=1&from_search=true&qid=BMXnK83th5&rank=1",
+      isbn: "9781857231588"
+    },
+    %Book{
+      category: "Science Fiction",
+      title: "The Robot Novels (trilogy)",
+      author: "Isaac Asimov",
+      url:
+        "https://www.goodreads.com/book/show/138551.The_Robot_Novels?from_search=true&from_srp=true&qid=ng2AQ4fIJO&rank=3",
+      isbn: "9780345331199"
     }
   ]
 
