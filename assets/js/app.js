96f832017b3c226b69df9cdbffa8bf0fa477e7b0
David Heimann <d@dmeh.net>
2021-12-08 21:32:41 -0500

    Remove sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ import Prism from "prismjs";
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss";
+import "../css/app.css";
 import "../css/prism-material-oceanic.css";
 
 // webpack automatically bundles all modules in your
